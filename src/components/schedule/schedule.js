7dfe8139bb54a4ef6f18f82d4448b3db6b6d563b
Parth Parulekar <psp3@rice.edu>
2019-05-26 21:15:46 -0700

    Changed text in 'Datathon Schedule' section and hyperlinked Duncan Hall on Google Maps. Edited answers in FAQs section.

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -6,8 +6,8 @@ const Schedule = () => (
     <div className='schedule-page'>
         <Title title="Datathon Schedule" width={[0.7, 0.4]} theme={false} />
         <div className='schedule-items'>
-            <h1 style={{fontWeight: 'normal'}}><img src='https://icon.now.sh/event/A7BBCE/50' alt=''/>Mark you calendars, the Rice Datathon will start Jan 24th and end Jan 25th 2020</h1>
-            <h1 style={{fontWeight: 'normal'}}><img src='https://icon.now.sh/pin/A7BBCE/50' alt=''/>The Datathon will be held in Duncan Hall on the Rice Campus</h1>
+            <h1 style={{fontWeight: 'normal'}}><img src='https://icon.now.sh/event/A7BBCE/50' alt=''/>Mark your calendars, the Rice Datathon will take place January 24-25, 2020</h1>
+            <h1 style={{fontWeight: 'normal'}}><img src='https://icon.now.sh/pin/A7BBCE/50' alt=''/>The Datathon will be held in <a href="https://goo.gl/maps/m3tKWrdj84y5RpY6A" target="_blank">Duncan Hall</a> on the Rice Campus</h1>
         </div>
     </div>
 )
